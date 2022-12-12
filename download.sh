1559cfa809dd72253a82b0527ade300af2ed55a2
pjdelgado <pedro.j.delgado@gmail.com>
2022-12-12 13:42:37 -0500

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+wget https://github.com/pjdelgado/bb/raw/main/airixss.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/anew.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/csprecon.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/dalfox.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/getjs.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/gf.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/golinkfinder.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/html-tool.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/katana.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/kxss.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/qsreplace.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/subover.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/subzy.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/unfurl.tar.gz
+wget https://github.com/pjdelgado/bb/raw/main/waybackurls.tar.gz
